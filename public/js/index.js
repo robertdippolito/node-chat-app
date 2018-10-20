9acac0b23fa61642b31442ceef95c374e0b300f1
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-10-20 15:49:09 -0400

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+  socket.emit('createMessage', {
+    from: 'jen@example.com',
+    text: 'hi there'
+  });
+});
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New message', message);
+});
--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'mike@example.com',
+    text: 'what is going on?',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (newMsg) => {
+    console.log('new message from', newMsg);
+  });
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   })
