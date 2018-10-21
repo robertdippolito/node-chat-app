8530a08e5ea672c18a7fedc81d2672cf26efc43a
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-10-21 18:22:14 -0400

    add geolocation capabilities and tests

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -24,11 +25,10 @@ io.on('connection', (socket) => {
     console.log('new message from', newMsg);
     io.emit('newMessage', generateMessage(newMsg.from, newMsg.text));
     callback('This is from the server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: newMsg.from,
-    //   text: newMsg.text,
-    //   createdAt: new Date().getTime()
-    // });
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -10,5 +10,16 @@ describe('generateMessage', () => {
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
     expect(typeof message.createdAt).toBe('number');
+  });
+});
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    let from = 'Admin';
+    let lat = 123;
+    let long = 456;
+    let coords = generateLocationMessage(from, lat, long);
+    expect(coords.from).toBe(from);
+    expect(coords.url).toBe(`https://www.google.com/maps?q=${lat},${long}`);
+    expect(typeof coords.createdAt).toBe('number')
   });
 });
