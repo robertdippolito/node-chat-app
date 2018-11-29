a53aeed67f0a0ceec4de0a2685f9435383883be4
robert <robert.e.dippolito@accenture.com>
2018-11-29 12:51:20 -0500

    send messages to only people in the same chat

--- server/server.js
+++ server/server.js
@@ -40,13 +40,18 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (newMsg, callback) => {
-    console.log('new message from', newMsg);
-    io.emit('newMessage', generateMessage(newMsg.from, newMsg.text));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(newMsg.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, newMsg.text));
+    }
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user){
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
